20c3cf12d7c6fe6d58b98a270606ce7c56a996a0
EstiaanBritz <82169901+madwizzard@users.noreply.github.com>
2023-09-28 19:12:19 +0200

    Creating first form

--- PSS ITWORKS/LogIn.cs
+++ PSS ITWORKS/LogIn.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace PSS_ITWORKS
 {
-    public partial class Form1 : Form
+    public partial class LogIn : Form
     {
-        public Form1()
+        public LogIn()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
