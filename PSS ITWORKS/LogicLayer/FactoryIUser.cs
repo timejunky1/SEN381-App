910265b2bbabf25999226eac63f456d9889f9441
Jaco <111895986+timejunky1@users.noreply.github.com>
2023-10-03 20:23:37 +0200

    Finalised Base for the factory and users, as well as adding the interfaces

--- PSS ITWORKS/LogicLayer/FactoryIUser.cs
+++ PSS ITWORKS/LogicLayer/FactoryIUser.cs
@@ -2,6 +2,6 @@
 {
     public interface FactoryIUser
     {
-         void ShowUserInterface();
+        void ShowUserInterface();
     }
 }
--- PSS ITWORKS/LogicLayer/LoginController.cs
+++ PSS ITWORKS/LogicLayer/LoginController.cs
@@ -8,5 +8,23 @@ namespace PSS_ITWORKS.LogicLayer
 {
     class LoginController
     {
+        private DatabaseAPI api;
+        private FactoryUserFactory factory;
+        public FactoryIUser Login(string Username, string Password)
+        {
+            //This Method will:
+            //set connection for api
+            //check credentials
+            //get authorization type of that user
+            //create user with factory
+            //Create a Login and add the login to the database
+            //return an instance of a user with the associated authorization
+            throw new NotImplementedException();
+        }
+
+        public void LogOut(string Username)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
