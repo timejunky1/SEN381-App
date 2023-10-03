e59fa23e2d1cb12700edaddc2682a004efc83774
Johan Lindeque <94356627+JohanLindeque@users.noreply.github.com>
2023-10-03 20:45:45 +0200

    Strategy update

---
+++ PSS ITWORKS/LogicLayer/StrategyManagement/StrategyJobManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSS_ITWORKS.LogicLayer
+{
+    class StrategyJobManager : IStrategyManagement
+    {
+
+        // connect to api ?
+        // void connectDB(); ?
+
+
+
+        public void Get()
+        {
+            // get something
+            messageBox.Show("Get something");
+        }
+
+        public void Create()
+        {
+            // create something
+            messageBox.Show("Create something");
+        }
+
+        public void Delete(int ID)
+        {
+            // delete something
+            messageBox.Show("Delete something");
+        }
+
+        public void Update()
+        {
+            // update something
+            messageBox.Show("Update something");
+        }
+
+        public void Connect(string myString)
+        {
+            // connect to something
+            messageBox.Show("Connect to or soemthing");
+        }
+    }
+}
