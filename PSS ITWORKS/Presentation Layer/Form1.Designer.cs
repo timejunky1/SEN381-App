4d654cff0a7d76f1eb422ede561eec0a2ac6ae71
Ronwynne <578580@student.belgiumcampus.ac.za>
2023-10-25 09:32:39 +0200

    Corrected format of the form

--- PSS ITWORKS/Presentation Layer/Form1.Designer.cs
+++ PSS ITWORKS/Presentation Layer/Form1.Designer.cs
@@ -29,25 +29,25 @@
         private void InitializeComponent()
         {
             this.Heading_pnl = new System.Windows.Forms.Panel();
-            this.PSS_img = new System.Windows.Forms.PictureBox();
             this.PSS_lbl = new System.Windows.Forms.Label();
+            this.PSS_img = new System.Windows.Forms.PictureBox();
             this.CreateServiceRequest_lbl = new System.Windows.Forms.Label();
             this.PersonalInfo_gbx = new System.Windows.Forms.GroupBox();
-            this.QueryInfo_gbx = new System.Windows.Forms.GroupBox();
-            this.CustomerName_lbl = new System.Windows.Forms.Label();
-            this.CustomerName_txt = new System.Windows.Forms.TextBox();
-            this.ContactNumber_txt = new System.Windows.Forms.TextBox();
-            this.ContactNumber_lbl = new System.Windows.Forms.Label();
             this.EmailAddress_txt = new System.Windows.Forms.TextBox();
             this.EmailAddress_lbl = new System.Windows.Forms.Label();
+            this.ContactNumber_txt = new System.Windows.Forms.TextBox();
+            this.ContactNumber_lbl = new System.Windows.Forms.Label();
+            this.CustomerName_txt = new System.Windows.Forms.TextBox();
+            this.CustomerName_lbl = new System.Windows.Forms.Label();
+            this.QueryInfo_gbx = new System.Windows.Forms.GroupBox();
+            this.Description_rbx = new System.Windows.Forms.RichTextBox();
+            this.Description_lbl = new System.Windows.Forms.Label();
+            this.ServiceType_cbx = new System.Windows.Forms.ComboBox();
+            this.PriorityLevel__cbx = new System.Windows.Forms.ComboBox();
             this.ServiceType_lbl = new System.Windows.Forms.Label();
             this.PriorityLevel_lbl = new System.Windows.Forms.Label();
             this.EquipmentSerialNumber_txt = new System.Windows.Forms.TextBox();
             this.EquipmentSerialNumber_lbl = new System.Windows.Forms.Label();
-            this.PriorityLevel__cbx = new System.Windows.Forms.ComboBox();
-            this.ServiceType_cbx = new System.Windows.Forms.ComboBox();
-            this.Description_lbl = new System.Windows.Forms.Label();
-            this.Description_rbx = new System.Windows.Forms.RichTextBox();
             this.Submit_btn = new System.Windows.Forms.Button();
             this.Close_btn = new System.Windows.Forms.Button();
             this.Heading_pnl.SuspendLayout();
@@ -63,8 +63,19 @@
             this.Heading_pnl.Controls.Add(this.PSS_img);
             this.Heading_pnl.Location = new System.Drawing.Point(-1, 1);
             this.Heading_pnl.Name = "Heading_pnl";
-            this.Heading_pnl.Size = new System.Drawing.Size(1056, 100);
+            this.Heading_pnl.Size = new System.Drawing.Size(1233, 100);
             this.Heading_pnl.TabIndex = 0;
+            // 
+            // PSS_lbl
+            // 
+            this.PSS_lbl.AutoSize = true;
+            this.PSS_lbl.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PSS_lbl.ForeColor = System.Drawing.Color.Black;
+            this.PSS_lbl.Location = new System.Drawing.Point(124, 24);
+            this.PSS_lbl.Name = "PSS_lbl";
+            this.PSS_lbl.Size = new System.Drawing.Size(373, 41);
+            this.PSS_lbl.TabIndex = 1;
+            this.PSS_lbl.Text = "Primiere Service Solution";
             // 
             // PSS_img
             // 
@@ -73,17 +84,6 @@
             this.PSS_img.Size = new System.Drawing.Size(101, 75);
             this.PSS_img.TabIndex = 0;
             this.PSS_img.TabStop = false;
-            // 
-            // PSS_lbl
-            // 
-            this.PSS_lbl.AutoSize = true;
-            this.PSS_lbl.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PSS_lbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(183)))), ((int)(((byte)(132)))));
-            this.PSS_lbl.Location = new System.Drawing.Point(124, 24);
-            this.PSS_lbl.Name = "PSS_lbl";
-            this.PSS_lbl.Size = new System.Drawing.Size(464, 50);
-            this.PSS_lbl.TabIndex = 1;
-            this.PSS_lbl.Text = "Primiere Service Solution";
             // 
             // CreateServiceRequest_lbl
             // 
@@ -106,10 +106,61 @@
             this.PersonalInfo_gbx.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.PersonalInfo_gbx.Location = new System.Drawing.Point(39, 174);
             this.PersonalInfo_gbx.Name = "PersonalInfo_gbx";
-            this.PersonalInfo_gbx.Size = new System.Drawing.Size(256, 246);
+            this.PersonalInfo_gbx.Size = new System.Drawing.Size(324, 410);
             this.PersonalInfo_gbx.TabIndex = 2;
             this.PersonalInfo_gbx.TabStop = false;
             this.PersonalInfo_gbx.Text = "Personal Information";
+            // 
+            // EmailAddress_txt
+            // 
+            this.EmailAddress_txt.Location = new System.Drawing.Point(26, 297);
+            this.EmailAddress_txt.Name = "EmailAddress_txt";
+            this.EmailAddress_txt.Size = new System.Drawing.Size(209, 27);
+            this.EmailAddress_txt.TabIndex = 5;
+            // 
+            // EmailAddress_lbl
+            // 
+            this.EmailAddress_lbl.AutoSize = true;
+            this.EmailAddress_lbl.ForeColor = System.Drawing.Color.Blue;
+            this.EmailAddress_lbl.Location = new System.Drawing.Point(22, 262);
+            this.EmailAddress_lbl.Name = "EmailAddress_lbl";
+            this.EmailAddress_lbl.Size = new System.Drawing.Size(104, 20);
+            this.EmailAddress_lbl.TabIndex = 4;
+            this.EmailAddress_lbl.Text = "Email Address";
+            // 
+            // ContactNumber_txt
+            // 
+            this.ContactNumber_txt.Location = new System.Drawing.Point(26, 191);
+            this.ContactNumber_txt.Name = "ContactNumber_txt";
+            this.ContactNumber_txt.Size = new System.Drawing.Size(209, 27);
+            this.ContactNumber_txt.TabIndex = 3;
+            // 
+            // ContactNumber_lbl
+            // 
+            this.ContactNumber_lbl.AutoSize = true;
+            this.ContactNumber_lbl.ForeColor = System.Drawing.Color.Blue;
+            this.ContactNumber_lbl.Location = new System.Drawing.Point(19, 158);
+            this.ContactNumber_lbl.Name = "ContactNumber_lbl";
+            this.ContactNumber_lbl.Size = new System.Drawing.Size(122, 20);
+            this.ContactNumber_lbl.TabIndex = 2;
+            this.ContactNumber_lbl.Text = "Contact Number";
+            // 
+            // CustomerName_txt
+            // 
+            this.CustomerName_txt.Location = new System.Drawing.Point(26, 85);
+            this.CustomerName_txt.Name = "CustomerName_txt";
+            this.CustomerName_txt.Size = new System.Drawing.Size(209, 27);
+            this.CustomerName_txt.TabIndex = 1;
+            // 
+            // CustomerName_lbl
+            // 
+            this.CustomerName_lbl.AutoSize = true;
+            this.CustomerName_lbl.ForeColor = System.Drawing.Color.Blue;
+            this.CustomerName_lbl.Location = new System.Drawing.Point(19, 54);
+            this.CustomerName_lbl.Name = "CustomerName_lbl";
+            this.CustomerName_lbl.Size = new System.Drawing.Size(119, 20);
+            this.CustomerName_lbl.TabIndex = 0;
+            this.CustomerName_lbl.Text = "Customer Name";
             // 
             // QueryInfo_gbx
             // 
@@ -124,114 +175,18 @@
             this.QueryInfo_gbx.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.QueryInfo_gbx.Location = new System.Drawing.Point(472, 174);
             this.QueryInfo_gbx.Name = "QueryInfo_gbx";
-            this.QueryInfo_gbx.Size = new System.Drawing.Size(583, 283);
+            this.QueryInfo_gbx.Size = new System.Drawing.Size(747, 410);
             this.QueryInfo_gbx.TabIndex = 3;
             this.QueryInfo_gbx.TabStop = false;
             this.QueryInfo_gbx.Text = "Query Information";
             // 
-            // CustomerName_lbl
+            // Description_rbx
             // 
-            this.CustomerName_lbl.AutoSize = true;
-            this.CustomerName_lbl.ForeColor = System.Drawing.Color.Blue;
-            this.CustomerName_lbl.Location = new System.Drawing.Point(22, 28);
-            this.CustomerName_lbl.Name = "CustomerName_lbl";
-            this.CustomerName_lbl.Size = new System.Drawing.Size(119, 20);
-            this.CustomerName_lbl.TabIndex = 0;
-            this.CustomerName_lbl.Text = "Customer Name";
-            // 
-            // CustomerName_txt
-            // 
-            this.CustomerName_txt.Location = new System.Drawing.Point(26, 47);
-            this.CustomerName_txt.Name = "CustomerName_txt";
-            this.CustomerName_txt.Size = new System.Drawing.Size(100, 27);
-            this.CustomerName_txt.TabIndex = 1;
-            // 
-            // ContactNumber_txt
-            // 
-            this.ContactNumber_txt.Location = new System.Drawing.Point(26, 123);
-            this.ContactNumber_txt.Name = "ContactNumber_txt";
-            this.ContactNumber_txt.Size = new System.Drawing.Size(100, 27);
-            this.ContactNumber_txt.TabIndex = 3;
-            // 
-            // ContactNumber_lbl
-            // 
-            this.ContactNumber_lbl.AutoSize = true;
-            this.ContactNumber_lbl.ForeColor = System.Drawing.Color.Blue;
-            this.ContactNumber_lbl.Location = new System.Drawing.Point(22, 104);
-            this.ContactNumber_lbl.Name = "ContactNumber_lbl";
-            this.ContactNumber_lbl.Size = new System.Drawing.Size(122, 20);
-            this.ContactNumber_lbl.TabIndex = 2;
-            this.ContactNumber_lbl.Text = "Contact Number";
-            // 
-            // EmailAddress_txt
-            // 
-            this.EmailAddress_txt.Location = new System.Drawing.Point(26, 191);
-            this.EmailAddress_txt.Name = "EmailAddress_txt";
-            this.EmailAddress_txt.Size = new System.Drawing.Size(100, 27);
-            this.EmailAddress_txt.TabIndex = 5;
-            // 
-            // EmailAddress_lbl
-            // 
-            this.EmailAddress_lbl.AutoSize = true;
-            this.EmailAddress_lbl.ForeColor = System.Drawing.Color.Blue;
-            this.EmailAddress_lbl.Location = new System.Drawing.Point(22, 172);
-            this.EmailAddress_lbl.Name = "EmailAddress_lbl";
-            this.EmailAddress_lbl.Size = new System.Drawing.Size(104, 20);
-            this.EmailAddress_lbl.TabIndex = 4;
-            this.EmailAddress_lbl.Text = "Email Address";
-            // 
-            // ServiceType_lbl
-            // 
-            this.ServiceType_lbl.AutoSize = true;
-            this.ServiceType_lbl.ForeColor = System.Drawing.Color.Blue;
-            this.ServiceType_lbl.Location = new System.Drawing.Point(15, 161);
-            this.ServiceType_lbl.Name = "ServiceType_lbl";
-            this.ServiceType_lbl.Size = new System.Drawing.Size(95, 20);
-            this.ServiceType_lbl.TabIndex = 10;
-            this.ServiceType_lbl.Text = "Service Type";
-            // 
-            // PriorityLevel_lbl
-            // 
-            this.PriorityLevel_lbl.AutoSize = true;
-            this.PriorityLevel_lbl.ForeColor = System.Drawing.Color.Blue;
-            this.PriorityLevel_lbl.Location = new System.Drawing.Point(15, 93);
-            this.PriorityLevel_lbl.Name = "PriorityLevel_lbl";
-            this.PriorityLevel_lbl.Size = new System.Drawing.Size(99, 20);
-            this.PriorityLevel_lbl.TabIndex = 8;
-            this.PriorityLevel_lbl.Text = "Priority Level";
-            // 
-            // EquipmentSerialNumber_txt
-            // 
-            this.EquipmentSerialNumber_txt.Location = new System.Drawing.Point(19, 47);
-            this.EquipmentSerialNumber_txt.Name = "EquipmentSerialNumber_txt";
-            this.EquipmentSerialNumber_txt.Size = new System.Drawing.Size(100, 27);
-            this.EquipmentSerialNumber_txt.TabIndex = 7;
-            // 
-            // EquipmentSerialNumber_lbl
-            // 
-            this.EquipmentSerialNumber_lbl.AutoSize = true;
-            this.EquipmentSerialNumber_lbl.ForeColor = System.Drawing.Color.Blue;
-            this.EquipmentSerialNumber_lbl.Location = new System.Drawing.Point(15, 28);
-            this.EquipmentSerialNumber_lbl.Name = "EquipmentSerialNumber_lbl";
-            this.EquipmentSerialNumber_lbl.Size = new System.Drawing.Size(186, 20);
-            this.EquipmentSerialNumber_lbl.TabIndex = 6;
-            this.EquipmentSerialNumber_lbl.Text = "Equipment Serial Number";
-            // 
-            // PriorityLevel__cbx
-            // 
-            this.PriorityLevel__cbx.FormattingEnabled = true;
-            this.PriorityLevel__cbx.Location = new System.Drawing.Point(19, 116);
-            this.PriorityLevel__cbx.Name = "PriorityLevel__cbx";
-            this.PriorityLevel__cbx.Size = new System.Drawing.Size(121, 28);
-            this.PriorityLevel__cbx.TabIndex = 12;
-            // 
-            // ServiceType_cbx
-            // 
-            this.ServiceType_cbx.FormattingEnabled = true;
-            this.ServiceType_cbx.Location = new System.Drawing.Point(19, 184);
-            this.ServiceType_cbx.Name = "ServiceType_cbx";
-            this.ServiceType_cbx.Size = new System.Drawing.Size(121, 28);
-            this.ServiceType_cbx.TabIndex = 13;
+            this.Description_rbx.Location = new System.Drawing.Point(283, 51);
+            this.Description_rbx.Name = "Description_rbx";
+            this.Description_rbx.Size = new System.Drawing.Size(458, 329);
+            this.Description_rbx.TabIndex = 15;
+            this.Description_rbx.Text = "";
             // 
             // Description_lbl
             // 
@@ -243,22 +198,67 @@
             this.Description_lbl.TabIndex = 14;
             this.Description_lbl.Text = "Description of Issue";
             // 
-            // Description_rbx
+            // ServiceType_cbx
             // 
-            this.Description_rbx.Location = new System.Drawing.Point(283, 51);
-            this.Description_rbx.Name = "Description_rbx";
-            this.Description_rbx.Size = new System.Drawing.Size(280, 195);
-            this.Description_rbx.TabIndex = 15;
-            this.Description_rbx.Text = "";
+            this.ServiceType_cbx.FormattingEnabled = true;
+            this.ServiceType_cbx.Location = new System.Drawing.Point(19, 296);
+            this.ServiceType_cbx.Name = "ServiceType_cbx";
+            this.ServiceType_cbx.Size = new System.Drawing.Size(209, 28);
+            this.ServiceType_cbx.TabIndex = 13;
+            // 
+            // PriorityLevel__cbx
+            // 
+            this.PriorityLevel__cbx.FormattingEnabled = true;
+            this.PriorityLevel__cbx.Location = new System.Drawing.Point(19, 190);
+            this.PriorityLevel__cbx.Name = "PriorityLevel__cbx";
+            this.PriorityLevel__cbx.Size = new System.Drawing.Size(209, 28);
+            this.PriorityLevel__cbx.TabIndex = 12;
+            // 
+            // ServiceType_lbl
+            // 
+            this.ServiceType_lbl.AutoSize = true;
+            this.ServiceType_lbl.ForeColor = System.Drawing.Color.Blue;
+            this.ServiceType_lbl.Location = new System.Drawing.Point(15, 262);
+            this.ServiceType_lbl.Name = "ServiceType_lbl";
+            this.ServiceType_lbl.Size = new System.Drawing.Size(95, 20);
+            this.ServiceType_lbl.TabIndex = 10;
+            this.ServiceType_lbl.Text = "Service Type";
+            // 
+            // PriorityLevel_lbl
+            // 
+            this.PriorityLevel_lbl.AutoSize = true;
+            this.PriorityLevel_lbl.ForeColor = System.Drawing.Color.Blue;
+            this.PriorityLevel_lbl.Location = new System.Drawing.Point(15, 158);
+            this.PriorityLevel_lbl.Name = "PriorityLevel_lbl";
+            this.PriorityLevel_lbl.Size = new System.Drawing.Size(99, 20);
+            this.PriorityLevel_lbl.TabIndex = 8;
+            this.PriorityLevel_lbl.Text = "Priority Level";
+            // 
+            // EquipmentSerialNumber_txt
+            // 
+            this.EquipmentSerialNumber_txt.Location = new System.Drawing.Point(19, 85);
+            this.EquipmentSerialNumber_txt.Name = "EquipmentSerialNumber_txt";
+            this.EquipmentSerialNumber_txt.Size = new System.Drawing.Size(209, 27);
+            this.EquipmentSerialNumber_txt.TabIndex = 7;
+            // 
+            // EquipmentSerialNumber_lbl
+            // 
+            this.EquipmentSerialNumber_lbl.AutoSize = true;
+            this.EquipmentSerialNumber_lbl.ForeColor = System.Drawing.Color.Blue;
+            this.EquipmentSerialNumber_lbl.Location = new System.Drawing.Point(15, 54);
+            this.EquipmentSerialNumber_lbl.Name = "EquipmentSerialNumber_lbl";
+            this.EquipmentSerialNumber_lbl.Size = new System.Drawing.Size(186, 20);
+            this.EquipmentSerialNumber_lbl.TabIndex = 6;
+            this.EquipmentSerialNumber_lbl.Text = "Equipment Serial Number";
             // 
             // Submit_btn
             // 
             this.Submit_btn.BackColor = System.Drawing.Color.MediumSeaGreen;
             this.Submit_btn.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Submit_btn.ForeColor = System.Drawing.Color.White;
-            this.Submit_btn.Location = new System.Drawing.Point(263, 487);
+            this.Submit_btn.Location = new System.Drawing.Point(282, 605);
             this.Submit_btn.Name = "Submit_btn";
-            this.Submit_btn.Size = new System.Drawing.Size(113, 38);
+            this.Submit_btn.Size = new System.Drawing.Size(211, 43);
             this.Submit_btn.TabIndex = 4;
             this.Submit_btn.Text = "Submit";
             this.Submit_btn.UseVisualStyleBackColor = false;
@@ -268,9 +268,9 @@
             this.Close_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(73)))), ((int)(((byte)(73)))));
             this.Close_btn.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Close_btn.ForeColor = System.Drawing.Color.White;
-            this.Close_btn.Location = new System.Drawing.Point(469, 487);
+            this.Close_btn.Location = new System.Drawing.Point(612, 605);
             this.Close_btn.Name = "Close_btn";
-            this.Close_btn.Size = new System.Drawing.Size(113, 38);
+            this.Close_btn.Size = new System.Drawing.Size(211, 43);
             this.Close_btn.TabIndex = 5;
             this.Close_btn.Text = "Close";
             this.Close_btn.UseVisualStyleBackColor = false;
@@ -279,7 +279,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(1231, 689);
             this.Controls.Add(this.Close_btn);
             this.Controls.Add(this.Submit_btn);
             this.Controls.Add(this.QueryInfo_gbx);
