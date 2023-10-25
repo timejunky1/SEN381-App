320c62e7c0d1f812eab2d6623b3fdfd7cdf05009
Ronwynne <578580@student.belgiumcampus.ac.za>
2023-10-25 13:04:30 +0200

    Created a form for updating a contract

---
+++ PSS ITWORKS/Presentation Layer/UpdateContract.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace PSS_ITWORKS.Presentation_Layer
+{
+    partial class UpdateContract
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UpdateContract_gbx = new System.Windows.Forms.GroupBox();
+            this.Close_btn = new System.Windows.Forms.Button();
+            this.Update_btn = new System.Windows.Forms.Button();
+            this.SLA_rtb = new System.Windows.Forms.RichTextBox();
+            this.Availability_cbx = new System.Windows.Forms.ComboBox();
+            this.Priority_txt = new System.Windows.Forms.TextBox();
+            this.Priority_lbl = new System.Windows.Forms.Label();
+            this.Cost_txt = new System.Windows.Forms.TextBox();
+            this.Cost_lbl = new System.Windows.Forms.Label();
+            this.Availability_lbl = new System.Windows.Forms.Label();
+            this.SLA_lbl = new System.Windows.Forms.Label();
+            this.Duration_txt = new System.Windows.Forms.TextBox();
+            this.Duration_lbl = new System.Windows.Forms.Label();
+            this.Heading_lbl = new System.Windows.Forms.Label();
+            this.Logo_img = new System.Windows.Forms.PictureBox();
+            this.Search_gbx = new System.Windows.Forms.GroupBox();
+            this.Search_lbl = new System.Windows.Forms.Label();
+            this.Search_txt = new System.Windows.Forms.TextBox();
+            this.Search_btn = new System.Windows.Forms.Button();
+            this.Delete_btn = new System.Windows.Forms.Button();
+            this.UpdateContract_gbx.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Logo_img)).BeginInit();
+            this.Search_gbx.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // UpdateContract_gbx
+            // 
+            this.UpdateContract_gbx.Controls.Add(this.Close_btn);
+            this.UpdateContract_gbx.Controls.Add(this.Update_btn);
+            this.UpdateContract_gbx.Controls.Add(this.SLA_rtb);
+            this.UpdateContract_gbx.Controls.Add(this.Availability_cbx);
+            this.UpdateContract_gbx.Controls.Add(this.Priority_txt);
+            this.UpdateContract_gbx.Controls.Add(this.Priority_lbl);
+            this.UpdateContract_gbx.Controls.Add(this.Cost_txt);
+            this.UpdateContract_gbx.Controls.Add(this.Cost_lbl);
+            this.UpdateContract_gbx.Controls.Add(this.Availability_lbl);
+            this.UpdateContract_gbx.Controls.Add(this.SLA_lbl);
+            this.UpdateContract_gbx.Controls.Add(this.Duration_txt);
+            this.UpdateContract_gbx.Controls.Add(this.Duration_lbl);
+            this.UpdateContract_gbx.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UpdateContract_gbx.Location = new System.Drawing.Point(14, 207);
+            this.UpdateContract_gbx.Name = "UpdateContract_gbx";
+            this.UpdateContract_gbx.Size = new System.Drawing.Size(1205, 470);
+            this.UpdateContract_gbx.TabIndex = 6;
+            this.UpdateContract_gbx.TabStop = false;
+            this.UpdateContract_gbx.Text = "Update Contract";
+            // 
+            // Close_btn
+            // 
+            this.Close_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(73)))), ((int)(((byte)(73)))));
+            this.Close_btn.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Close_btn.ForeColor = System.Drawing.Color.White;
+            this.Close_btn.Location = new System.Drawing.Point(463, 401);
+            this.Close_btn.Name = "Close_btn";
+            this.Close_btn.Size = new System.Drawing.Size(211, 43);
+            this.Close_btn.TabIndex = 13;
+            this.Close_btn.Text = "Close";
+            this.Close_btn.UseVisualStyleBackColor = false;
+            // 
+            // Update_btn
+            // 
+            this.Update_btn.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.Update_btn.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Update_btn.ForeColor = System.Drawing.Color.White;
+            this.Update_btn.Location = new System.Drawing.Point(41, 401);
+            this.Update_btn.Name = "Update_btn";
+            this.Update_btn.Size = new System.Drawing.Size(211, 43);
+            this.Update_btn.TabIndex = 12;
+            this.Update_btn.Text = "Update";
+            this.Update_btn.UseVisualStyleBackColor = false;
+            // 
+            // SLA_rtb
+            // 
+            this.SLA_rtb.Location = new System.Drawing.Point(463, 75);
+            this.SLA_rtb.Name = "SLA_rtb";
+            this.SLA_rtb.Size = new System.Drawing.Size(694, 294);
+            this.SLA_rtb.TabIndex = 11;
+            this.SLA_rtb.Text = "";
+            // 
+            // Availability_cbx
+            // 
+            this.Availability_cbx.FormattingEnabled = true;
+            this.Availability_cbx.Items.AddRange(new object[] {
+            "Available",
+            "Unavailable"});
+            this.Availability_cbx.Location = new System.Drawing.Point(41, 336);
+            this.Availability_cbx.Name = "Availability_cbx";
+            this.Availability_cbx.Size = new System.Drawing.Size(209, 33);
+            this.Availability_cbx.TabIndex = 10;
+            // 
+            // Priority_txt
+            // 
+            this.Priority_txt.Location = new System.Drawing.Point(41, 162);
+            this.Priority_txt.Name = "Priority_txt";
+            this.Priority_txt.Size = new System.Drawing.Size(209, 31);
+            this.Priority_txt.TabIndex = 9;
+            // 
+            // Priority_lbl
+            // 
+            this.Priority_lbl.AutoSize = true;
+            this.Priority_lbl.ForeColor = System.Drawing.Color.Blue;
+            this.Priority_lbl.Location = new System.Drawing.Point(36, 124);
+            this.Priority_lbl.Name = "Priority_lbl";
+            this.Priority_lbl.Size = new System.Drawing.Size(68, 25);
+            this.Priority_lbl.TabIndex = 8;
+            this.Priority_lbl.Text = "Priority";
+            // 
+            // Cost_txt
+            // 
+            this.Cost_txt.Location = new System.Drawing.Point(41, 255);
+            this.Cost_txt.Name = "Cost_txt";
+            this.Cost_txt.Size = new System.Drawing.Size(209, 31);
+            this.Cost_txt.TabIndex = 7;
+            // 
+            // Cost_lbl
+            // 
+            this.Cost_lbl.AutoSize = true;
+            this.Cost_lbl.ForeColor = System.Drawing.Color.Blue;
+            this.Cost_lbl.Location = new System.Drawing.Point(36, 214);
+            this.Cost_lbl.Name = "Cost_lbl";
+            this.Cost_lbl.Size = new System.Drawing.Size(48, 25);
+            this.Cost_lbl.TabIndex = 6;
+            this.Cost_lbl.Text = "Cost";
+            // 
+            // Availability_lbl
+            // 
+            this.Availability_lbl.AutoSize = true;
+            this.Availability_lbl.ForeColor = System.Drawing.Color.Blue;
+            this.Availability_lbl.Location = new System.Drawing.Point(36, 307);
+            this.Availability_lbl.Name = "Availability_lbl";
+            this.Availability_lbl.Size = new System.Drawing.Size(97, 25);
+            this.Availability_lbl.TabIndex = 4;
+            this.Availability_lbl.Text = "Availability";
+            // 
+            // SLA_lbl
+            // 
+            this.SLA_lbl.AutoSize = true;
+            this.SLA_lbl.ForeColor = System.Drawing.Color.Blue;
+            this.SLA_lbl.Location = new System.Drawing.Point(458, 37);
+            this.SLA_lbl.Name = "SLA_lbl";
+            this.SLA_lbl.Size = new System.Drawing.Size(204, 25);
+            this.SLA_lbl.TabIndex = 2;
+            this.SLA_lbl.Text = "Service Level Agreement";
+            // 
+            // Duration_txt
+            // 
+            this.Duration_txt.Location = new System.Drawing.Point(41, 75);
+            this.Duration_txt.Name = "Duration_txt";
+            this.Duration_txt.Size = new System.Drawing.Size(209, 31);
+            this.Duration_txt.TabIndex = 1;
+            // 
+            // Duration_lbl
+            // 
+            this.Duration_lbl.AutoSize = true;
+            this.Duration_lbl.ForeColor = System.Drawing.Color.Blue;
+            this.Duration_lbl.Location = new System.Drawing.Point(36, 37);
+            this.Duration_lbl.Name = "Duration_lbl";
+            this.Duration_lbl.Size = new System.Drawing.Size(153, 25);
+            this.Duration_lbl.TabIndex = 0;
+            this.Duration_lbl.Text = "Contract Duration";
+            // 
+            // Heading_lbl
+            // 
+            this.Heading_lbl.AutoSize = true;
+            this.Heading_lbl.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Heading_lbl.Location = new System.Drawing.Point(123, 37);
+            this.Heading_lbl.Name = "Heading_lbl";
+            this.Heading_lbl.Size = new System.Drawing.Size(584, 41);
+            this.Heading_lbl.TabIndex = 5;
+            this.Heading_lbl.Text = "Welcome back <name> <surname> (title) ";
+            // 
+            // Logo_img
+            // 
+            this.Logo_img.Location = new System.Drawing.Point(14, 16);
+            this.Logo_img.Name = "Logo_img";
+            this.Logo_img.Size = new System.Drawing.Size(101, 75);
+            this.Logo_img.TabIndex = 4;
+            this.Logo_img.TabStop = false;
+            // 
+            // Search_gbx
+            // 
+            this.Search_gbx.Controls.Add(this.Delete_btn);
+            this.Search_gbx.Controls.Add(this.Search_btn);
+            this.Search_gbx.Controls.Add(this.Search_txt);
+            this.Search_gbx.Controls.Add(this.Search_lbl);
+            this.Search_gbx.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Search_gbx.Location = new System.Drawing.Point(14, 108);
+            this.Search_gbx.Name = "Search_gbx";
+            this.Search_gbx.Size = new System.Drawing.Size(1205, 79);
+            this.Search_gbx.TabIndex = 7;
+            this.Search_gbx.TabStop = false;
+            this.Search_gbx.Text = "Search Contract";
+            // 
+            // Search_lbl
+            // 
+            this.Search_lbl.AutoSize = true;
+            this.Search_lbl.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Search_lbl.ForeColor = System.Drawing.Color.Blue;
+            this.Search_lbl.Location = new System.Drawing.Point(38, 38);
+            this.Search_lbl.Name = "Search_lbl";
+            this.Search_lbl.Size = new System.Drawing.Size(95, 20);
+            this.Search_lbl.TabIndex = 0;
+            this.Search_lbl.Text = "Search by ID:";
+            // 
+            // Search_txt
+            // 
+            this.Search_txt.Location = new System.Drawing.Point(150, 31);
+            this.Search_txt.Name = "Search_txt";
+            this.Search_txt.Size = new System.Drawing.Size(209, 31);
+            this.Search_txt.TabIndex = 1;
+            // 
+            // Search_btn
+            // 
+            this.Search_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.Search_btn.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Search_btn.ForeColor = System.Drawing.Color.White;
+            this.Search_btn.Location = new System.Drawing.Point(463, 25);
+            this.Search_btn.Name = "Search_btn";
+            this.Search_btn.Size = new System.Drawing.Size(211, 43);
+            this.Search_btn.TabIndex = 14;
+            this.Search_btn.Text = "Search";
+            this.Search_btn.UseVisualStyleBackColor = false;
+            // 
+            // Delete_btn
+            // 
+            this.Delete_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(73)))), ((int)(((byte)(73)))));
+            this.Delete_btn.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Delete_btn.ForeColor = System.Drawing.Color.White;
+            this.Delete_btn.Location = new System.Drawing.Point(749, 25);
+            this.Delete_btn.Name = "Delete_btn";
+            this.Delete_btn.Size = new System.Drawing.Size(211, 43);
+            this.Delete_btn.TabIndex = 14;
+            this.Delete_btn.Text = "Delete";
+            this.Delete_btn.UseVisualStyleBackColor = false;
+            // 
+            // UpdateContract
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1231, 689);
+            this.Controls.Add(this.Search_gbx);
+            this.Controls.Add(this.UpdateContract_gbx);
+            this.Controls.Add(this.Heading_lbl);
+            this.Controls.Add(this.Logo_img);
+            this.Name = "UpdateContract";
+            this.Text = "UpdateContract";
+            this.UpdateContract_gbx.ResumeLayout(false);
+            this.UpdateContract_gbx.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Logo_img)).EndInit();
+            this.Search_gbx.ResumeLayout(false);
+            this.Search_gbx.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox UpdateContract_gbx;
+        private System.Windows.Forms.Button Close_btn;
+        private System.Windows.Forms.Button Update_btn;
+        private System.Windows.Forms.RichTextBox SLA_rtb;
+        private System.Windows.Forms.ComboBox Availability_cbx;
+        private System.Windows.Forms.TextBox Priority_txt;
+        private System.Windows.Forms.Label Priority_lbl;
+        private System.Windows.Forms.TextBox Cost_txt;
+        private System.Windows.Forms.Label Cost_lbl;
+        private System.Windows.Forms.Label Availability_lbl;
+        private System.Windows.Forms.Label SLA_lbl;
+        private System.Windows.Forms.TextBox Duration_txt;
+        private System.Windows.Forms.Label Duration_lbl;
+        private System.Windows.Forms.Label Heading_lbl;
+        private System.Windows.Forms.PictureBox Logo_img;
+        private System.Windows.Forms.GroupBox Search_gbx;
+        private System.Windows.Forms.Button Delete_btn;
+        private System.Windows.Forms.Button Search_btn;
+        private System.Windows.Forms.TextBox Search_txt;
+        private System.Windows.Forms.Label Search_lbl;
+    }
+}
