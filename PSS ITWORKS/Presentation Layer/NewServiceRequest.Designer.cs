8d75c7c97896666f6577744a96d50f76b9554417
Zander Rosslee <zanderrosslee@gmail.com>
2023-10-31 18:03:50 +0200

    Modify Jobs UI done

--- PSS ITWORKS/Presentation Layer/NewServiceRequest.Designer.cs
+++ PSS ITWORKS/Presentation Layer/NewServiceRequest.Designer.cs
@@ -305,7 +305,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(856, 450);
+            this.ClientSize = new System.Drawing.Size(856, 408);
             this.Controls.Add(this.PSS_img);
             this.Controls.Add(this.PSS_lbl);
             this.Controls.Add(this.Close_btn);
