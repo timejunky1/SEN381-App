e09b1586205f89f000129318997003591bb6d924
Ronwynne <578580@student.belgiumcampus.ac.za>
2023-11-01 16:46:15 +0200

    Added code to create new service request on newservicerequestForm linked to the call form

--- PSS ITWORKS/Presentation Layer/NewServiceRequestForm.cs
+++ PSS ITWORKS/Presentation Layer/NewServiceRequestForm.cs
@@ -9,15 +9,17 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using PSS_ITWORKS.LogicLayer;
 using PSS_ITWORKS.Presentation_Layer;
+using System.Data.SqlClient;
 
 namespace PSS_ITWORKS.Presentation_Layer
 {
     public partial class ServiceRequestForm : Form
     {
+        SqlConnection con;
         StrategyCallManagement callManagement;
         EntityJob job;
         BindingSource bs;
-
+        
         public ServiceRequestForm()
         {
             InitializeComponent();
@@ -25,7 +27,23 @@ namespace PSS_ITWORKS.Presentation_Layer
 
         private void ServiceRequestForm_Load(object sender, EventArgs e)
         {
+            /////Populate Priority combo box
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SELECT service_id,DISTINCT priority, DISTINCT title FROM service");
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            cmd.ExecuteNonQuery();
+            con.Close();
 
+            PriorityLevel__cbx.DataSource = ds.Tables[0];
+            PriorityLevel__cbx.DisplayMember = "priority";
+            PriorityLevel__cbx.ValueMember = "service_id";
+
+            /////Populate Service Type combo box
+            ServiceType_cbx.DataSource = ds.Tables[0];
+            ServiceType_cbx.DisplayMember = "title";
+            ServiceType_cbx.ValueMember = "service_id";
         }
 
         private void Submit_btn_Click(object sender, EventArgs e)
@@ -39,9 +57,8 @@ namespace PSS_ITWORKS.Presentation_Layer
             int priority = (int)PriorityLevel__cbx.Items[selectedIndex];
             int ServiceTypeIndex = ServiceType_cbx.SelectedIndex;
             ServiceType_cbx.SelectedItem.ToString();
-            int serviceType = (int)ServiceType_cbx.Items[ServiceTypeIndex];
+            int serviceType = (int)ServiceType_cbx.ValueMember[ServiceTypeIndex];
             string desc = "Equipment Serial Number: " + equipSerialNo + " " + Description_rbx.Text;
-
 
             job = new EntityJob(customerID, serviceType, desc);
             callManagement.Create(job);
